eb2cb3b3039a3c31173aaa08e35eecb7a805a9ce
dzyabtest <dzyab@mail.ru>
2023-06-27 23:13:57 +0300

    Установка шаблона

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+// TODO: write your code here
+import sum, { matcher } from './basic';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+console.log(matcher([
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+]));
---
+++ src/js/basic.js
@@ -0,0 +1,11 @@
+export default function sum(items) {
+  let result = 0;
+  for (const item of items) {
+    result += item;
+  }
+  return result;
+}
+
+export function matcher(chars) {
+  return chars.sort((prev, next) => next.health - prev.health);
+}
